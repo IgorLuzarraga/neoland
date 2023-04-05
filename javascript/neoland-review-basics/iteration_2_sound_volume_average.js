e64df35ee30b46aa61193d2ff7fbd894beda9496
Igor <iluzarraga@gmail.com>
2023-04-05 17:32:44 +0200

    iteration #2 sound volume average

---
+++ javascript/neoland-review-basics/iteration_2_sound_volume_average.js
@@ -0,0 +1,46 @@
+const users = [
+    {   name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {   name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {   name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {   name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+const soundVolumeAverage = (users) => {
+    let counter = 0
+    let totalVolume = 0
+
+    for(const user of users) {
+        for(const favorites in user.favoritesSounds) {
+            totalVolume = totalVolume + user.favoritesSounds[favorites].volume
+            counter++
+        }
+    }
+
+    return totalVolume/counter
+}
+
+console.log(`Sound Vulume Average: ${soundVolumeAverage(users)}`)
