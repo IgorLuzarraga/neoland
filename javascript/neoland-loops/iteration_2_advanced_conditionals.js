336f3a5001beabc698b74513364dbb1a29cce8bf
Igor <iluzarraga@gmail.com>
2023-04-05 09:56:48 +0200

    add immutability for the array alumns2

--- javascript/neoland-loops/iteration_2_advanced_conditionals.js
+++ javascript/neoland-loops/iteration_2_advanced_conditionals.js
@@ -16,6 +16,32 @@ const testPassed = (studentArr) => {
     return studentArr
 }
 
-console.log(alumns)
-console.log(testPassed(alumns))
-console.log(alumns)
+//console.log(testPassed(alumns))
+
+// ----------------------------------------------//
+// without mutate the array alumns
+
+const alumns2 = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+    {name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+    {name: 'Juan Miranda', T1: false, T2: true, T3: true},
+    {name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+    {name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+const testPassed2 = (studentArr) => {
+    // deep copy of the studentArr array
+    const studentArrCopy = structuredClone(studentArr)
+
+    studentArrCopy.forEach(student => {
+        ((student.T1 && student.T2) || (student.T1 && student.T3) || (student.T2 && student.T3)) 
+            ? student.isApproved = true
+            : student.isApproved = false
+    })
+
+    return studentArrCopy
+}
+
+console.log("before: \n\n", alumns2)
+console.log(testPassed2(alumns2))
+console.log("after: \n\n", alumns2) // The alumn2 array didn't change
