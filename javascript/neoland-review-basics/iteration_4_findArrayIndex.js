1b9dbfb8f981ce94cee85faa95c9d7d18914bda7
Igor <iluzarraga@gmail.com>
2023-04-12 23:34:58 +0200

    add a second method to find the index element in the array

--- javascript/neoland-review-basics/iteration_4_findArrayIndex.js
+++ javascript/neoland-review-basics/iteration_4_findArrayIndex.js
@@ -4,3 +4,15 @@ const findArrayIndex = (array, text) =>
     array.indexOf(text)
 
 console.log(findArrayIndex(animals, "Ajolote"))
+
+const findArrayIndex2 = (array, text) => {
+    let counter = 0
+    for(const el of array) {
+        if(el.includes(text)) {
+            return counter
+        }
+        counter++
+    }
+}
+
+console.log(findArrayIndex2(animals, "Ajolote"))
