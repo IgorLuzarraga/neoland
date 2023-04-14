511f9f25a9be3007f945d6e33064dbce877a18f1
Igor <iluzarraga@gmail.com>
2023-04-15 00:53:38 +0200

    Show ordered (by date ) list of products

--- javascript/extra_exercises/5.yoda/exercise-3/exercise-3.js
+++ javascript/extra_exercises/5.yoda/exercise-3/exercise-3.js
@@ -24,3 +24,111 @@
 // ejemplo ``http://localhost:3000/products/2``. De esta 
 // forma podremos obtener ya toda la información y pintarla 
 // en el html.
+
+const showProducHeader = (product) => {
+  const div = document.createElement('div')
+  const pId = document.createElement('p')
+  const pQuantity = document.createElement('p')
+
+  pId.textContent = `Product id: ${product.productId}`
+  pQuantity.textContent = `Product quantity: ${product.quantity}`
+
+  div.appendChild(pId)
+  div.appendChild(pQuantity)
+
+  document.body.appendChild(div)
+}
+
+const showProductInfo = (product) => {
+  const pInfo = document.createElement('p')
+
+  pInfo.textContent = `Product name: ${product.name}`
+
+  document.body.appendChild(pInfo)
+}
+
+const showOrderInfo = (order) => {
+    const div = document.createElement('div')
+    const pDate = document.createElement('p')
+    const pId = document.createElement('p')
+
+    pDate.textContent = `Order date: ${order.date}`
+    pId.textContent = `Order Id: ${order.id}`
+
+    div.appendChild(pDate)
+    div.appendChild(pId)
+
+    document.body.appendChild(div)
+}
+
+const showOrderSeparator = () => {
+  const hr = document.createElement('hr')
+
+  hr.style.height = '8px'
+  hr.style.background='black'
+  hr.style.border = '3px solid black'
+
+  document.body.appendChild(hr)
+}
+
+const showProductSeparator = () => {
+  const hr = document.createElement('hr')
+
+  hr.style.height = '1px'
+  hr.style.background='black'
+  hr.style.border = '1px solid black'
+
+  document.body.appendChild(hr)
+}
+
+const showOrderInfoSeparator = () => {
+  const hr = document.createElement('hr')
+
+  hr.style.height = '2px'
+  hr.style.background='black'
+  hr.style.border = '1px solid black'
+
+  document.body.appendChild(hr)
+}
+
+const sortOrdersByDate = (orders) => 
+  orders.sort((a, b) => {
+        const date1 = new Date(a.date)
+        const date2 = new Date(b.date)
+        return (date2 - date1)
+      })
+
+const showOrdersInDOM = async () => {
+  try {
+    const response = await fetch(`http://localhost:3000/orders`);
+    const orders = await response.json();
+
+    const ordersSorted = sortOrdersByDate(orders)
+
+    for (let i = 0; i < ordersSorted.length; i++) {
+      showOrderInfo(ordersSorted[i])
+      showOrderInfoSeparator()
+
+      let products = ordersSorted[i].products
+
+      for (let j = 0; j < products.length; j++) {
+        const productId = products[j].productId
+
+        // use the product id to fetch additional data
+        const producttResponse = await fetch(`http://localhost:3000/products/${productId}`);
+        const productInfo = await producttResponse.json();
+        
+        showProducHeader(products[j])
+  
+        showProductInfo(productInfo)
+
+        showProductSeparator()
+      }
+
+      showOrderSeparator()
+    }
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+  }
+}
+
