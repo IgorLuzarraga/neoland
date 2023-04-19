4861ca1c1c87d09f7a138a1af0af022ad6992a81
Igor <iluzarraga@gmail.com>
2023-04-19 15:04:05 +0200

    First commit

---
+++ React/Pokedex/src/App.tsx
@@ -0,0 +1,18 @@
+
+import "./App.css";
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="TitleSection">
+        <h1>Pokédex</h1>
+        <input type="text" />
+        <button>Search Pokémon</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
