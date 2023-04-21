5993601697df709c15d892b4c4e641878817818d
Igor <iluzarraga@gmail.com>
2023-04-21 15:04:28 +0200

    Lego replication using Tailwind

--- Web Replication/2. LegoTW/tailwind.config.js
+++ Web Replication/2. LegoTW/tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '72': '18rem',
+        '1/10': '10%'
+      },
+      colors: {
+        'header-color': '#ffcf01',
+        'background-color': '#ffffff',
+        'primary-color': '#111111',
+      },
+      fontSize: {
+        headline: '4rem',
+        'typography-h1': '3.5rem',
+        'typography-h2': '2.25rem',
+        'typography-h3': '1.5rem',
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
+
+
 
