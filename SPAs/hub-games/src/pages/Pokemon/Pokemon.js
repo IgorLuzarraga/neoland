4eb8570fdcef91a18b41141c2bb71d9c8e39818b
Igor <iluzarraga@gmail.com>
2023-04-30 18:31:41 +0200

    added the pokemon searcher (by name)

--- SPAs/hub-games/src/pages/Pokemon/Pokemon.js
+++ SPAs/hub-games/src/pages/Pokemon/Pokemon.js
@@ -5,6 +5,7 @@ import {Loading} from "../../components/Loading/Loading"
 
 const template = () => `
     <div class="spinner"></div>
+    <div id="containerPokemonSearcher"></div>
     <div class="galleryPokemon"></div>
     `
 const templateFigure = (pokemon) => `
@@ -13,6 +14,16 @@ const templateFigure = (pokemon) => `
         <h2>${pokemon.name}</h2>
     </figure>
 `
+
+const templatePokemonSearcher = () => `
+    <div id='pokemonSearcher'>
+        <span>Pokemon by name:</span>
+        <div id="input-btn">
+            <input id="inputSearcher"></input>
+            <button id="btnSearcher">Search</button>
+        </div>
+    </div>
+    `
 
 const addSpinner = () => {
     // Render a spinner
@@ -24,6 +35,12 @@ const removeSpinner = () => {
     const spinnerContainer = document.querySelector(".spinner")
     spinnerContainer.innerHTML = ""
 }
+
+const addPokemonSearcher = () => 
+    document
+        .querySelector("#containerPokemonSearcher")
+        .innerHTML = templatePokemonSearcher()
+
 
 const addPokemonToGalley = (pokemon) => 
     document
@@ -46,6 +63,23 @@ const getPokemons2 =  () => {
         })
 }
 
+const handleSearchPokemon = async (name) => {
+    document.querySelector('.galleryPokemon').innerHTML = ""
+    const pokemons = await service_getPokemons()
+    pokemons
+        .filter(pokemon => pokemon.name.includes(name))
+        .forEach(addPokemonToGalley)
+
+}
+
+const addListeners = () => {
+    document
+        .querySelector('#btnSearcher')
+        .addEventListener('click', () => {
+            handleSearchPokemon(document.querySelector('#inputSearcher').value)
+        })
+}
+
 export const printTemplate = () => {
     // insert the page's Html
     document.querySelector('main').innerHTML = template()
@@ -56,11 +90,15 @@ export const printTemplate = () => {
     // Delay of 2 seconds to check if the spinner is shown
     // Remove it to go faster
     setTimeout(() => {
+        addPokemonSearcher()
+
         // fetch the pokemons
         getPokemons()
 
         // remove spinner after fetch
         removeSpinner()
+
+        addListeners()
     }, 2000)
     
 
