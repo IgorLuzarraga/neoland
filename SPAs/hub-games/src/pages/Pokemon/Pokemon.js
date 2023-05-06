069d285702ee757201cef7b8768dbdde59a32a60
Igor <iluzarraga@gmail.com>
2023-05-06 20:30:35 +0200

    Improved pokemon search, now the filter function dosen't call the service, uses previous data from the server

--- SPAs/hub-games/src/pages/Pokemon/Pokemon.js
+++ SPAs/hub-games/src/pages/Pokemon/Pokemon.js
@@ -3,6 +3,8 @@ import { service_getPokemons } from '../../services/pokemon.service'
 //import { service_getPokemons } from '../../services/pokemon.service2'
 import {Loading} from "../../components/Loading/Loading"
 
+let pokemonDataFromService = []
+
 const template = () => `
     <div class="spinner"></div>
     <div id="containerPokemonSearcher"></div>
@@ -42,15 +44,15 @@ const addPokemonSearcher = () =>
         .innerHTML = templatePokemonSearcher()
 
 
-const addPokemonToGalley = (pokemon) => 
+const addPokemonToGallery = (pokemon) => 
     document
         .querySelector('.galleryPokemon')
         .innerHTML +=  templateFigure(pokemon)
 
 // Version 1 -- uses --> services/pokemon.service
 const getPokemons = async () => {
-    const pokemons = await service_getPokemons()
-    pokemons.forEach(addPokemonToGalley)
+    pokemonDataFromService = await service_getPokemons()
+    pokemonDataFromService.forEach(addPokemonToGallery)
 }
 
 // Version 2 -- uses --> services/pokemon.service2
@@ -58,11 +60,15 @@ const getPokemons2 =  () => {
     const pokemonsPromise = service_getPokemons()
         pokemonsPromise.forEach(promise => {
             promise
-                .then(addPokemonToGalley)
+                .then((pokemon) => {
+                    pokemonDataFromService.push(pokemon) 
+                    addPokemonToGallery(pokemon)
+                })
                 .catch(error => console.log(error))
         })
 }
 
+// ----------------------------- Filters (Start) ----------------------
 const byName = name => pokemon =>
     pokemon
         .name
@@ -71,35 +77,53 @@ const byName = name => pokemon =>
 
 const filterPokemonByName = async (name) => {
     document.querySelector('.galleryPokemon').innerHTML = ""
-    const pokemons = await service_getPokemons()
-    pokemons
+    pokemonDataFromService
         .filter(byName(name))
-        .forEach(addPokemonToGalley)
+        .forEach(addPokemonToGallery)
 }
 
-const addListeners = (pokemonTypes) => {
+const filterPokemonByType = (type) => {
+    const filterData = dataServicePokemon.filter((pokemon) =>
+        pokemon.type[0].type.name.toLowerCase().includes(filtro.toLowerCase())
+      );
+
+      if (filterData.length === 0) {
+        const filterData = dataServicePokemon.filter((pokemon) =>
+          pokemon.type[1]?.type.name
+            .toLowerCase()
+            .includes(filtro.toLowerCase())
+        );
+        createAndPrintFigure(filterData);
+      } else {
+        createAndPrintFigure(filterData);
+      }
+}
+// ----------------------------- Filters (End) ----------------------
+
+//const addListeners = (pokemonTypes) => {
+const addListeners = () => {
     document
         .querySelector('#btnSearcher')
         .addEventListener('click', () => {
             filterPokemonByName(document.querySelector('#inputSearcher').value)
         })
 
-    pokemonTypes.forEach((type) => {
-        const buttonType = document.querySelector(`.${type}`)
+    // pokemonTypes.forEach((type) => {
+    //     const buttonType = document.querySelector(`.${type}`)
     
-        buttonType.addEventListener("click", (e) => {
-            filterPokemonByType(type, "type")
-        })
-    })
+    //     buttonType.addEventListener("click", (e) => {
+    //         filterPokemonByType(type)
+    //     })
+    // })
 }
 
-const printPokemonTypesBtns = (pokemonTypes) => 
-    pokemonTypes.forEach((type) => {
-      const idCustom = `button${type[0].toUpperCase() + type.slice(1)}`;
-      const buttonType = `<button class="buttonFilter ${type}" id=>${type}</button>`;
-      const filterButton = document.getElementById("filterButton");
-      filterButton.innerHTML += buttonType;
-    })
+// const printPokemonTypesBtns = (pokemonTypes) => 
+//     pokemonTypes.forEach((type) => {
+//       const idCustom = `button${type[0].toUpperCase() + type.slice(1)}`;
+//       const buttonType = `<button class="buttonFilter ${type}" id=>${type}</button>`;
+//       const filterButton = document.getElementById("filterButton");
+//       filterButton.innerHTML += buttonType;
+//     })
 
 export const printTemplate = () => {
     // insert the page's Html
@@ -118,6 +142,8 @@ export const printTemplate = () => {
 
         // remove spinner after fetch
         removeSpinner()
+
+        //printPokemonTypesBtns()
 
         addListeners()
     }, 2000)
