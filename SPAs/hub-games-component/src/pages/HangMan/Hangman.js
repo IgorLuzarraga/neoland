acee35ed3be7d62006bc7187001f975024c21cac
Igor <iluzarraga@gmail.com>
2023-05-15 09:42:45 +0200

    changed Hangman layout and styles

--- SPAs/hub-games-component/src/pages/HangMan/Hangman.js
+++ SPAs/hub-games-component/src/pages/HangMan/Hangman.js
@@ -9,12 +9,13 @@ const template = () => `
   <div id="hangman-container-main">
     <div id="hangman-container">
       <div id="hangman-title">Hangman</div>
-      <div id="handman-word">
+      <div id="handman-word-and-display-container">
         <img id="hangman-image" src="https://res.cloudinary.com/dtkuzehsp/image/upload/v1683670674/hangman1_in5wob.jpg"/>
-      </div>
-      <div id="handman-display">
-        <input id="hangman-input-letter" type="textarea" placeholder="A letter please" maxlength="1"/>
-        <button id="hangman-try-btn">Try</button>
+        <div id="handman-display">
+          <div id="handman-word"></div>
+          <input id="hangman-input-letter" type="textarea" placeholder="A letter please" maxlength="1"/>
+          <button id="hangman-try-btn">Try</button>
+        </div>
       </div>
     </div>
   </div>
