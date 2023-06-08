6a3df8fa9f88f7dec77c7d6f9b6fc25c4d6d972e
Igor <iluzarraga@gmail.com>
2023-06-08 17:52:11 +0200

    Added to the cards that shows the App (apps that come from mobileDev) info: description and image

--- React/React Final Project (Experiments)/version4/src/components/CardApp_inMobileDev/CardApp_inMobileDev.jsx
+++ React/React Final Project (Experiments)/version4/src/components/CardApp_inMobileDev/CardApp_inMobileDev.jsx
@@ -9,13 +9,18 @@ export const CardApp_inMobileDev = ({ app }) =>
         {showAppInfo(app)}
     </div>
 
-const showAppInfo = (app) =>
-    <div className='cardAppInMobileDev-Info'>
-        {/* <p>app id: {app._id}</p> */}
-        <p>app name: {app.appName}</p>
-        <p>app category: {app.category}</p>
-        <p>app codeLanguages: {app.codeLanguages}</p>
-        <p>app appSize: {app.appSize} Mbytes</p>
-    </div>
+const showAppInfo = (app) => {
+    console.log('showAppInfo -> app: ', app)
+    return (
+        <div className='cardAppInMobileDev-Info'>
+            {/* <p>app id: {app._id}</p> */}
+            <p>app name: {app.appName}</p>
+            <p>app category: {app.category}</p>
+            <p>app codeLanguages: {app.codeLanguages}</p>
+            <p>app appSize: {app.appSize} Mbytes</p>
+            <p>description: {app.description}</p>
+        </div>
+    )
+}
 
 export default CardApp_inMobileDev;
